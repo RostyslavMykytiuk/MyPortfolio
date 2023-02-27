6badf0f8849d40fbb8b0fb01a563c647e12e9c84
Rostyslav Mykytiuk <rostyslav.mykytiuk.vp.2020@lpnu.ua>
2023-02-27 18:34:07 +0200

    created all components and imported them inb App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+//export all components
+import { Contact } from './components/contact/Contact';
+import { Experience } from './components/experience/Experience';
+import { Header } from './components/header/Header';
+import { Navbar } from './components/navbar/Navbar';
+import { About } from './components/about/About';
+import { Services } from './components/services/Services';
+import { Portfolio } from './components/portfolio/Portfolio';
+import { Footer } from "./components/footer/Footer"
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Navbar/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
